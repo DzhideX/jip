d7ed3ce631216760720831df709df2764a1dc450
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-05-20 13:18:35 +0200

    Add more comprehensive tokenization to search index

--- site/lib/index.ts
+++ site/lib/index.ts
@@ -1,5 +1,14 @@
 const { Index } = require("flexsearch");
 
-const index = new Index("performance");
+// const index = new Index("memory");
+
+// This option indexes every possible word combination, resulting in a much better search experience.
+// However, it also results in a much larger index. Due to the smaller number of JIPs at the moment.
+// If the index starts getting too large, we can revisit this in the future.
+// Link to docs: https://github.com/nextapps-de/flexsearch#tokenizer-prefix-search
+
+const index = new Index({
+  tokenize: "full",
+});
 
 export default index;

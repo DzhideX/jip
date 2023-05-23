085c2f9b2036a708411959cd34d51f8345cfbf18
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-05-23 11:40:43 +0200

    Initial implementation of item search dropdown list

--- site/components/layout.tsx
+++ site/components/layout.tsx
@@ -4,14 +4,21 @@ import Link from "next/link";
 import { useQuery } from "@tanstack/react-query";
 
 import { useDebounce } from "@/util/useDebounce";
+import { JipId } from "@/lib/files";
 
 import styles from "@/styles/layout.module.css";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const SearchDropdown = ({ jipIds }: { jipIds: Array<JipId>}) => (
+  <div className={styles.searchDropdown}>
+    {jipIds.map((jipId) => <div className={styles.searchDropdownItem}>{jipId}</div>)}
+  </div>
+)
+
+const Search = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const debouncedSearch = useDebounce<React.ChangeEvent<HTMLInputElement>>((e) => setSearchQuery(e.target.value), 400);
 
-  const { isLoading, error, data } = useQuery({ queryKey: ['searchQuery', searchQuery], queryFn: async () => {
+  const { isLoading, error, data } = useQuery<{ results: Array<JipId> }>({ queryKey: ['searchQuery', searchQuery], queryFn: async () => {
     const res = await fetch(`api/search?searchQuery=${searchQuery}`);
     return res.json();
    }});
@@ -19,6 +26,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   if(!isLoading && !error && data) {
     console.log(data);
   }
+
+  return (
+    <div className={styles.searchWrapper}>
+      <input type="text" className={styles.search} onChange={debouncedSearch} placeholder="Search.." />
+      {/* {isLoading && <div>Loading...</div>} */}
+      {!isLoading && !error && data?.results && data.results.length > 0 && <SearchDropdown jipIds={data.results} />}
+    </div>
+  )
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className={styles.container}>
@@ -30,7 +48,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <Link href="/" className={styles.heading}>
           Joystream Improvement Proposal Portal
         </Link>
-        <input type="text" className={styles.input} onChange={debouncedSearch} placeholder="Search.." />
+        <Search />
       </header>
       <main>{children}</main>
     </div>
